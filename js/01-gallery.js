283001bed2e912ec78f01044e3e446872afc42dd
AlinaShklyaruk <i.charge92@gmail.com>
2022-10-03 00:49:58 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryEl = document.querySelector('div.gallery');
+
+function createGalleryItemsMarkup(items) {
+    return items.map(item => {
+        `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${items.preview}"
+      data-source="${items.original}"
+      alt="${items.description}"
+    />
+  </a>
+</div>`
+    }).join('');
+};
+
+const listItemsMarkup = createGalleryItemsMarkup(galleryItems);
+console.log(listItemsMarkup);
+galleryEl.innerHTML = listItemsMarkup;
+
 console.log(galleryItems);
